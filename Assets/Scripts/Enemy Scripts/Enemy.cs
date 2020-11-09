0fc7ea6f694cb26455a439a4cd324a9d714d6fab
Jiminy Cao <jiminy.cao@gmail.com>
2020-11-08 21:15:21 -0500

    Linked health bars to character healths

--- Assets/Scripts/Enemy Scripts/Enemy.cs
+++ Assets/Scripts/Enemy Scripts/Enemy.cs
@@ -5,10 +5,12 @@ using UnityEngine;
 public class Enemy : MonoBehaviour
 {
     public int HP;
+    public HealthBar healthBar;
 
     public void OnHit(int damage)
     {
         HP -= damage;
+        healthBar.SetHealth(HP);
         if (HP <= 0)
         {
             Destroy(gameObject);
--- Assets/Scripts/Enemy Scripts/State Behaviour Scripts/BerserkerBehaviour.cs
+++ Assets/Scripts/Enemy Scripts/State Behaviour Scripts/BerserkerBehaviour.cs
@@ -19,6 +19,7 @@ public class BerserkerBehaviour : Enemy
     // Start is called before the first frame update
     void Awake()
     {
+        healthBar.SetMaxHealth(HP);
         navMeshAgent = this.GetComponent<NavMeshAgent>();
         navMeshAgent.Warp(this.transform.position);
     }
